cfc108e3ddda74f17baec7fb6e3f33d5a87baffe
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2025-05-11 15:28:19 +0530

    Project

---
+++ Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+
+namespace Repository.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=Tatva@123;Database=Test");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.BoxOfficeCollection).HasColumnType("character varying");
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.ImdbRating).HasPrecision(3, 2);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Repository/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int ReleaseYear { get; set; }
+
+    public string BoxOfficeCollection { get; set; } = null!;
+
+    public decimal ImdbRating { get; set; }
+}
